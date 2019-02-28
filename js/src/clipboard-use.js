cc66913643197cd5ef91b7f3bbeefd27baffa68b
baixian <1325165625@qq.com>
2019-02-28 18:32:38 +0800

    重构一下

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Feb 28 2019 18:24:16 GMT+0800 (GMT+08:00)
+// build time:Thu Feb 28 2019 18:32:20 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
